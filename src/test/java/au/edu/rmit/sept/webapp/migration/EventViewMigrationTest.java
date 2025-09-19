574911793f05efde043e732492726f7d7cb18906
anthony-vo <s3951749@student.rmit.edu.au>
2025-09-20 05:44:48 +1000

    Add migration file to pass the test. Fixed sql queries to be H2-safe

--- src/test/java/au/edu/rmit/sept/webapp/migration/EventViewMigrationTest.java
+++ src/test/java/au/edu/rmit/sept/webapp/migration/EventViewMigrationTest.java
@@ -18,8 +18,8 @@ public class EventViewMigrationTest {
 
   @Test
   void eventsTable_should_haveNewColumns() {
-    List<Map<String, Object>> columns = jdbc.queryForList("SHOW COLUMNS FROM events");
-    List<String> columnNames = columns.stream().map(m -> m.get("FIELD").toString().toLowerCase()).toList();
+    List<Map<String, Object>> columns = jdbc.queryForList("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'EVENTS'");
+    List<String> columnNames = columns.stream().map(m -> m.get("COLUMN_NAME").toString().toLowerCase()).toList();
 
     assertTrue(columnNames.contains("detailed_description"));
     assertTrue(columnNames.contains("agenda"));
