86e55e8b4eef6a5b27969ab2e0e10f81c7b2d0e7
JensenLy <s3977367@student.rmit.edu.au>
2025-10-08 18:33:51 +1100

    updated report table with status

--- src/main/resources/db/migration/V6__report_info.sql
+++ src/main/resources/db/migration/V6__report_info.sql
@@ -5,9 +5,10 @@ CREATE TABLE reports (
   user_id BIGINT NOT NULL,
   event_id BIGINT NOT NULL,
   note VARCHAR(1000),
+  status VARCHAR(50) NOT NULL CHECK (status IN ('open', 'under_review', 'resolved')),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
   CONSTRAINT fk_report_user FOREIGN KEY (user_id) REFERENCES users(user_id),
-  CONSTRAINT fk_report_event FOREIGN KEY (event_id) REFERENCES events(event_id),
-  CONSTRAINT uc_report_event UNIQUE (user_id, event_id)  -- prevent duplicate report
+  CONSTRAINT fk_report_event FOREIGN KEY (event_id) REFERENCES events(event_id)
+  -- CONSTRAINT uc_report_event UNIQUE (user_id, event_id)  -- prevent duplicate report
 );
