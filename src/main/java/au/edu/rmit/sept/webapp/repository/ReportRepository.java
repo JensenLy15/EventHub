1bd7eef3bfca69221eba3802bce4d0a3987c4740
JensenLy <s3977367@student.rmit.edu.au>
2025-10-08 21:33:03 +1100

    report feature model and repository added

---
+++ src/main/java/au/edu/rmit/sept/webapp/repository/ReportRepository.java
@@ -0,0 +1,41 @@
+package au.edu.rmit.sept.webapp.repository;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import au.edu.rmit.sept.webapp.model.Report;
+
+@Repository
+public class ReportRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReportRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    
+    private static final RowMapper<Report> MAPPER = (ResultSet rs, int rowNum) ->
+        new Report(
+            rs.getLong("report_id"),
+            rs.getLong("user_id"),
+            rs.getLong("event_id"),
+            rs.getString("note"),
+            rs.getTimestamp("created_at").toLocalDateTime()
+        );
+    
+    public List<Report> getAllReports() { 
+        String sql = "SELECT * FROM reports";
+        List<Report> reports = jdbcTemplate.query(sql, MAPPER);
+        return reports;
+    }
+    
+    public boolean addReport(Report rp) {
+        String sql = "INSERT INTO reports (user_id, event_id, note, status, created_at) VALUES (?, ?, ?, ?, ?)";
+        boolean status = jdbcTemplate.update(sql, rp.getUserId(), rp.getEventId(), rp.getNote(), rp.getStatus(), rp.getCreatedAt()) > 0;
+        return status;
+    }
+}
